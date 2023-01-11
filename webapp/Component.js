139aa49c45e6a4e44c6b9b67d62c797077dfaedf
iamjpa <jack.pawar999@gmail.com>
2023-01-11 19:20:27 +0000

    GIT Comment

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.case.pporders.pporders.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
